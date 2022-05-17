4a619e1c34e43eb22aaa99a8128dfa65b76806dd
Yusuke Moriizumi <makkoiisme@gmail.com>
2022-05-17 15:06:21 +0900

    fizzbuzzControllerを追加

---
+++ src/main/java/com/example/demo/FizzBuzzController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FizzBuzzController {
+
+	@RequestMapping(value = "/apply", method = RequestMethod.POST)
+	public ModelAndView apply(@RequestParam("num") int num, ModelAndView mv) {
+
+		String result = "";
+		if (num != 0) {
+
+			if (num % 3 == 0) {
+				result += "Fizz ";
+			}
+			if (num % 5 == 0) {
+				result += "Buzz ";
+			}
+			if (num % 7 == 0) {
+				result += "Ass ";
+			}
+		}
+		mv.addObject("result", result);
+		mv.addObject("num", num);
+
+		mv.setViewName("index");
+		return mv;
+	}
+
+}
